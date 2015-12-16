1353336bc989c402ba47926ce26b77a5bcd3e874
heiheiha <1137029429@qq.com>
2015-12-16 18:56:44 +0800

    最大公约数和最大公倍数
    输入俩个正整数，利用中间变量temp，用辗除法，知道b为0为止，求出这俩个正数的最小公倍数和最小公约数
    while(b!=0)
    {
    temp=a%b;
    a=b;
    b=temp;
    }

--- multiple/main.cpp
+++ multiple/main.cpp
@@ -10,6 +10,23 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a,b,num1,num2,temp;
+    printf("please input two numbers:\n");
+    scanf("%d,%d",&num1,&num2);
+    if(num1)
+    {
+        temp=num1;
+        num1=num2;
+        num2=temp;
+    }
+    a=num1;b=num2;
+    while(b!=0)
+    {
+        temp=a%b;
+        a=b;
+        b=temp;
+    }
+    printf("gongyueshu:%d\n",a);
+    printf("gongbeishu:%d\n",num1*num2/a);
     return 0;
 }
